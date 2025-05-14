aa6a96b395a7756a557bffda2109596365f3e671
Rakesh Kumar <rakeshk@audaxlabs.com>
2025-05-14 23:12:17 +0530

    no-6 done

--- index_low_arr.js
+++ index_low_arr.js
@@ -53,3 +53,50 @@
 
 // console.log("Max",max);
 // console.log("Min",min);
+
+// 4.Reverse an array (in-place)
+// const arr = [2, 3, 4, 3, 4, 5, 6, 4, 4, 7, 8, 9];
+
+    // Using defined method
+    // let newArr=arr.reverse()
+    // method 1
+    // let reversedArr=[];
+    // for(let i=arr.length-1;i>=0;i--){
+    //     reversedArr.push(arr[i]) 
+    //     console.log(arr[i])
+    // }
+    // console.log(reversedArr);                                       
+   
+// 5.Check if two arrays are equal (same elements and order)
+// const arr1 = [2, 3, 4, 3, 4, 5, 6, 4, 4, 7, 8, 9];
+// const arr2 = [2, 3, 4, 3, 4, 5, 6, 4, 4, 7,9,8];
+// function arrayAreEqual(arr1,arr2){
+// if(arr1.length !==arr2.length){
+//     return "Length of array is not equal."
+// }
+// for(let i=0; i<arr1.length;i++){
+//     if(arr1[i] !== arr2[i]){
+//         return "Arrays are not equal in same order."
+//     }
+    
+// }
+// return "Arrays are equal"
+// }
+// console.log(arrayAreEqual(arr1,arr2))
+
+// 6.Find the second largest number in an array
+const arr1 = [2, 3, 4, 3, 4, 5, 6, 4, 4, 7, 8, 9];
+function secondLargestNumber(arr) {
+    let max = arr1[0];
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    let secondLargest=arr1[0];
+    for(let i=0; i<max ;i++){
+        secondLargest=arr[i]
+    }
+    return secondLargest
+}
+console.log(secondLargestNumber(arr1));
